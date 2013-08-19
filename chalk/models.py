35dcaa65f5b5f2d607b1290f7df04966338213d3
Kevin Dias <kevin@kevindias.com>
2013-08-19 14:57:12 -0700

    Added docstrings and model help text.

--- chalk/models.py
+++ chalk/models.py
@@ -7,6 +7,9 @@ from django.db import models
 from docutils.core import publish_parts
 
 
+# Docutils config options.
+# See: http://docutils.sourceforge.net/docs/user/config.html
+# TODO: Make this a setting.
 DOCUTILS_OVERRIDES = {
     'syntax_highlight': 'short',
     'initial_header_level': 2,
@@ -15,15 +18,26 @@ DOCUTILS_OVERRIDES = {
 
 
 class Article(models.Model):
+    """
+    A chalk article or blog post.
+
+    The content_html and excerpt_html fields are populated from the
+    reStructuredText in content and excerpt. This HTML is regenerated
+    on every model save unless the protect_html flag is set, in which
+    case the HTML fields will not be changed automatically.
+
+    """
     author = models.ForeignKey(User)
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     published = models.BooleanField()
     content = models.TextField()
     excerpt = models.TextField(blank=True)
-    protect_html = models.BooleanField()
-    content_html = models.TextField(blank=True)
-    excerpt_html = models.TextField(blank=True)
+    protect_html = models.BooleanField(help_text='If checked, Content HTML and '
+                                       'Excerpt HTML will not be overwritten '
+                                       'by generated HTML')
+    content_html = models.TextField('content HTML', blank=True)
+    excerpt_html = models.TextField('excerpt HTML', blank=True)
     publication_date = models.DateField(default=date.today())
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
@@ -38,6 +52,12 @@ class Article(models.Model):
         return reverse('view_article', kwargs={'slug': self.slug})
     
     def generate_html(self):
+        """
+        Generate HTML from the content and excerpt fields.
+
+        Returns the generated HTML as a (content, excerpt) tuple.
+
+        """
         content = self.content
         excerpt = self.excerpt
 
@@ -51,6 +71,13 @@ class Article(models.Model):
         return (content_html, excerpt_html)
 
     def save(self, *args, **kwargs):
+        """
+        Populate content_html and excerpt_html and save the model.
+
+        If the protect_html flag is set, leave content_html and excerpt_html
+        untouched.
+
+        """
         if not self.protect_html:
             content_html, excerpt_html = self.generate_html()
             self.content_html = content_html
--- chalk/views.py
+++ chalk/views.py
@@ -4,9 +4,21 @@ from .models import Article
 
 
 class ArticleList(ListView):
+    """
+    List all articles by publication date descending.
+
+    The default template extends the 'container' block of 'base.html'
+    and is meant mostly as an example. Override 'chalk/article_list.html'
+    to use your own template.
+
+    Articles are provided to the template context in the 'article_list'
+    variable.
+
+    """
     model = Article
 
     def get_queryset(self):
+        """Non-staff users only see published articles."""
         qs = self.model.objects.all()
         if not self.request.user.is_staff:
             qs = qs.filter(published=True)
@@ -14,9 +26,21 @@ class ArticleList(ListView):
 
 
 class ArticleView(DetailView):
+    """
+    Show a detailed view of a single article.
+
+    The default template extends the 'container' block of 'base.html'
+    and is meant mostly as an example. Override 'chalk/article_detail.html'
+    to use your own template.
+
+    You can access the article being viewed through the 'article' variable
+    in the template context.
+
+    """
     model = Article
 
     def get_queryset(self):
+        """Non-staff users only see published articles."""
         qs = self.model.objects.all()
         if not self.request.user.is_staff:
             qs = qs.filter(published=True)
