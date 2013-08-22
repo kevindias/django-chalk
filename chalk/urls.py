a9fe1ef0438f4246c4d0d6096726caac8e5d8ba4
Kevin Dias <kevin@kevindias.com>
2013-08-21 18:32:52 -0700

    Added index view for all feeds.
    This may be superfluous, but it offers a simple way to give subscribers
    a choice between Atom and RSS without have to include extra buttons or
    links.

--- chalk/urls.py
+++ chalk/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 from .feeds import ArticleAtomFeed, ArticleRssFeed
 from .views import ArticleView, ArticleList
@@ -6,6 +7,8 @@ from .views import ArticleView, ArticleList
 
 urlpatterns = patterns('',
     url(r'^$', ArticleList.as_view(), name='list_articles'),
+    url(r'^feed/$', TemplateView.as_view(template_name='chalk/feed_list.html'),
+        name='list_feeds'),
     url(r'^feed/rss/$', ArticleRssFeed(), name='rss_feed'),
     url(r'^feed/atom/$', ArticleAtomFeed(), name='atom_feed'),
     url(r'^(?P<slug>[\w-]+)/$', ArticleView.as_view(), name='view_article'),
