b42da1fa00c532a245fa604d677deb7c9ae79985
Kevin Dias <kevin@kevindias.com>
2013-08-22 16:03:49 -0700

    Added comments describing use of feed settings.

--- chalk/settings.py
+++ chalk/settings.py
@@ -1,5 +1,10 @@
 from django.conf import settings
 
 
-FEED_TITLE = getattr(settings, 'CHALK_FEED_TITLE', 'Article Feed')
-FEED_DESCRIPTION = getattr(settings, 'CHALK_FEED_DESCRIPTION', 'All Articles')
+# Used as the title for RSS and Atom feeds of articles.
+# Set CHALK_FEED_TITLE in your settings.py to override.
+FEED_TITLE = getattr(settings, 'CHALK_FEED_TITLE', 'Article Feed1')
+
+# Used as the feed description (RSS) or subtitle (Atom) for article feeds.
+# Set CHALK_FEED_DESCRIPTION in your settings.py to override.
+FEED_DESCRIPTION = getattr(settings, 'CHALK_FEED_DESCRIPTION', 'All Articles1')
