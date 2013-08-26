8e7a241c432ea7f6796465aec1a39c461c7291f9
Kevin Dias <kevin@kevindias.com>
2013-08-25 17:05:48 -0700

    Updated package manifest
    Templates and fixtures weren't being included properly.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='django-chalk',
-      version='0.1.1',
+      version='0.1.2',
       author='Kevin Dias',
       author_email='kevin@kevindias.com',
       description='Simple reStructuredText blogging for Django',
